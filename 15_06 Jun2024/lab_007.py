6a5c1c03480df4af38c7a430377528bdae9e09e7
Ankur <ankurpal200@gmail.com>
2024-06-15 17:00:17 +0530

    hello

---
+++ 15_06 Jun2024/lab_007.py
@@ -0,0 +1,24 @@
+# Assignment operator
+
+name = "Ankur"
+print(name)
+
+#Unary Operator
+
+
+age = +95
+my_bank_balance = -100
+print(age)
+print(my_bank_balance)
+
+# Not operator
+is_married = True
+print(not is_married)
+
+# is operator
+
+a = 5
+b = -10.4
+print(a == b)
+
+
